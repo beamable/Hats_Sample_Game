497df6b06262cca575ac62437aaf57f8572e98b8
Chris Hanna <cdhannaphone@gmail.com>
2021-05-19 05:06:23 -0400

    character selection

--- Assets/Scripts/HatsUnity/CharacterBehaviour.cs
+++ Assets/Scripts/HatsUnity/CharacterBehaviour.cs
@@ -1,9 +1,44 @@
+using System.Threading.Tasks;
+using Beamable.UI.Scripts;
+using HatsContent;
+using HatsCore;
 using UnityEngine;
 
 namespace HatsUnity
 {
    public class CharacterBehaviour : MonoBehaviour
    {
+      [Header("Prefab References")]
+      public SpriteRenderer HatRendererPrefab;
 
+      [Header("Internal References")]
+      public Transform HatAnchor;
+
+      [Header("Internals")]
+      [ReadOnly]
+      [SerializeField]
+      private Sprite HatSprite;
+
+      [ReadOnly]
+      [SerializeField]
+      private HatContent HatContent;
+
+
+      public async Task SetHat(HatContent hatContent)
+      {
+         HatSprite = await hatContent.icon.LoadSprite();
+
+         ClearHat();
+         var hat = Instantiate(HatRendererPrefab, HatAnchor);
+         hat.sprite = HatSprite;
+      }
+
+      public void ClearHat()
+      {
+         for (var i = 0; i < HatAnchor.childCount; i++)
+         {
+            Destroy(HatAnchor.GetChild(i).gameObject);
+         }
+      }
    }
 }
--- Assets/Scripts/HatsUnity/UI/CharacterPanelController.cs
+++ Assets/Scripts/HatsUnity/UI/CharacterPanelController.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beamable.Common.Shop;
 using Beamable.Stats;
 using HatsContent;
+using HatsCore;
 using HatsUnity;
 using TMPro;
 using UnityEngine;
@@ -9,12 +12,16 @@ using UnityEngine.UI;
 
 public class CharacterPanelController : MonoBehaviour
 {
+    [Header("Content References")]
+    public StoreRef StoreRef;
 
     [Header("Prefab References")]
     public CharacterOptionBehaviour CharacterOptionPrefab;
+    public HatOptionBehaviour HatOptionPrefab;
 
     [Header("UI References")]
     public RectTransform CharacterOptionContainer;
+    public RectTransform HatOptionContainer;
     public TextMeshProUGUI HintText;
     public TextMeshProUGUI SelectedCharacterText;
     public GameObject LoadingSpinner;
@@ -22,37 +29,68 @@ public class CharacterPanelController : MonoBehaviour
     public RawImage RenderPreview;
     public TMP_InputField AliasInputField;
     public StatBehaviour AliasStatBehaviour;
+    public Button HatsButton;
+    public Button CharactersButton;
+
+    public RectTransform CharacterOptionScroller;
+    public RectTransform HatOptionScroller;
 
     [Header("Live Preview References")]
     public RenderTexture PreviewTexture;
     public Transform PreviewPrefabContainer;
+
+    [Header("Internals")]
+    [ReadOnly]
+    [SerializeField]
+    private HatContent _selectedHat;
+    [ReadOnly]
+    [SerializeField]
+    private CharacterContent _selectedCharacter;
+    [ReadOnly]
+    [SerializeField]
+    private CharacterBehaviour _selectedPrefab;
+
+    [ReadOnly]
+    [SerializeField]
+    private Vector2 _hatsOffset;
+    [ReadOnly]
+    [SerializeField]
+    private Vector2 _charactersOffset;
+
+    private Vector2 _hatsOffsetVel, _characterOffsetVel;
     // Start is called before the first frame update
     async void Start()
     {
         ClearOptions();
         ClearPreview();
+        ShowCharacters();
+
+        HatsButton.onClick.AddListener(ShowHats);
+        CharactersButton.onClick.AddListener(ShowCharacters);
 
         LoadingSpinner.SetActive(true);
         PreviewSpinner.SetActive(true);
-        HintText.text = "Loading Characters...";
+        HintText.text = "Fetching Content ...";
 
         AliasInputField.gameObject.SetActive(false);
         AliasStatBehaviour.OnStatReceived.AddListener(alias => AliasInputField.SetTextWithoutNotify(alias));
         AliasInputField.onEndEdit.AddListener(alias => AliasStatBehaviour.Write(alias));
 
         SelectedCharacterText.text = "Loading";
-        var selectedCharacter = await PlayerInventory.GetSelectedCharacter();
-        var selectedPrefab = await selectedCharacter.Prefab.SafeResolve();
-        ShowPreview(selectedCharacter, selectedPrefab);
+        _selectedCharacter = await PlayerInventory.GetSelectedCharacter();
+        _selectedPrefab = await _selectedCharacter.Prefab.SafeResolve();
+        _selectedHat = await PlayerInventory.GetSelectedHat();
+        ShowPreview(_selectedCharacter, _selectedPrefab, _selectedHat);
         PreviewSpinner.SetActive(false);
 
         await AliasStatBehaviour.Read();
         AliasInputField.gameObject.SetActive(true);
 
-
-
-        // load up the player's inventory, and show all the available options...
+        // load up the player's inventory, and show all the available characters...
         var characters = await PlayerInventory.GetAvailableCharacters();
+        // load up the player's inventory, and show all the available hats....
+        var hats = await PlayerInventory.GetAvailableHats();
+
         LoadingSpinner.SetActive(false);
         HintText.text = "Select your Characters";
 
@@ -62,15 +100,57 @@ public class CharacterPanelController : MonoBehaviour
             instance.SetOption(character);
             instance.OnSelected.AddListener(() =>
             {
-                Select(instance);
+                SelectCharacter(instance);
             });
         }
+        foreach (var hat in hats)
+        {
+            var instance = Instantiate(HatOptionPrefab, HatOptionContainer);
+            instance.SetOption(hat);
+            instance.OnSelected.AddListener(() =>
+            {
+                SelectHat(instance);
+            });
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        RenderPreview.color = Color.white;
         RenderPreview.texture = PreviewTexture;
+
+        HatOptionScroller.anchoredPosition =
+            Vector2.SmoothDamp(HatOptionScroller.anchoredPosition, _hatsOffset, ref _hatsOffsetVel, .1f);
+        CharacterOptionScroller.anchoredPosition =
+            Vector2.SmoothDamp(CharacterOptionScroller.anchoredPosition, _charactersOffset, ref _characterOffsetVel, .1f);
+    }
+
+    public async Task GetListings()
+    {
+        var beamable = await Beamable.API.Instance;
+        var shop = await beamable.CommerceService.GetCurrent(StoreRef.Id);
+        foreach (var listing in shop.listings)
+        {
+            listing.
+        }
+    }
+
+    public void ShowCharacters()
+    {
+        HatsButton.gameObject.SetActive(true);
+        CharactersButton.gameObject.SetActive(false);
+        _hatsOffset = new Vector2(Screen.width, HatOptionScroller.anchoredPosition.y);
+        _charactersOffset = new Vector2(0, HatOptionScroller.anchoredPosition.y);
+    }
+
+    public void ShowHats()
+    {
+        HatsButton.gameObject.SetActive(false);
+        CharactersButton.gameObject.SetActive(true);
+        _charactersOffset = new Vector2(-Screen.width, HatOptionScroller.anchoredPosition.y);
+        _hatsOffset = new Vector2(0, HatOptionScroller.anchoredPosition.y);
     }
 
     void ClearOptions()
@@ -78,6 +158,11 @@ public class CharacterPanelController : MonoBehaviour
         for (var i = 0; i < CharacterOptionContainer.childCount; i++)
         {
             Destroy(CharacterOptionContainer.GetChild(i).gameObject);
+        }
+
+        for (var i = 0; i < HatOptionContainer.childCount; i++)
+        {
+            Destroy(HatOptionContainer.GetChild(i).gameObject);
         }
     }
 
@@ -89,20 +174,29 @@ public class CharacterPanelController : MonoBehaviour
         }
     }
 
-    void ShowPreview(CharacterContent character, CharacterBehaviour prefab)
+    void ShowPreview(CharacterContent character, CharacterBehaviour prefab, HatContent selectedHat)
     {
         ClearPreview();
-        Instantiate(prefab, PreviewPrefabContainer);
+        var characterBehaviour = Instantiate(prefab, PreviewPrefabContainer);
+        var hatTask = characterBehaviour.SetHat(selectedHat);
         SelectedCharacterText.text = character.Display;
 
     }
 
-    public async void Select(CharacterOptionBehaviour option)
+    public async void SelectCharacter(CharacterOptionBehaviour option)
     {
-
+        _selectedCharacter = option.Character;
         var setTask = PlayerInventory.SetSelectedCharacter(option.Character);
-        var prefab = await option.Character.Prefab.SafeResolve();
-        ShowPreview(option.Character, prefab);
+        _selectedPrefab = await option.Character.Prefab.SafeResolve();
+        ShowPreview(_selectedCharacter, _selectedPrefab, _selectedHat);
+        await setTask;
+    }
+
+    public async void SelectHat(HatOptionBehaviour option)
+    {
+        _selectedHat = option.Hat;
+        var setTask = PlayerInventory.SetSelectedHat(option.Hat);
+        ShowPreview(_selectedCharacter, _selectedPrefab, _selectedHat);
         await setTask;
     }
 
