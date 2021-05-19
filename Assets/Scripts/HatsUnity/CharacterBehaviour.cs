cb08a320313ee6c9c2366901f2f5f885e2f78ca4
Jeff Rosenberg <jeff0rosenberg@gmail.com>
2021-05-18 20:53:33 -0700

    Created unified animator controller for characters and moved them into their own folders. Added Hit and Move animations for all characters. Adjusted AngryPig Hit sprite sheet to match color, and adjusted FatBird Idle animations. Added character movement to PlayerController and CharacterBehaviour.

--- Assets/Scripts/HatsUnity/CharacterBehaviour.cs
+++ Assets/Scripts/HatsUnity/CharacterBehaviour.cs
@@ -4,6 +4,34 @@ namespace HatsUnity
 {
    public class CharacterBehaviour : MonoBehaviour
    {
+		private readonly int MoveTrigger = Animator.StringToHash("Move");
+		private readonly int HitTrigger = Animator.StringToHash("Hit");
 
+		[SerializeField]
+		private Animator _animator;
+
+		[SerializeField]
+		private SpriteRenderer _spriteRenderer;
+
+		public void Move()
+		{
+			if(_animator)
+			{
+				_animator.SetTrigger(MoveTrigger);
+			}
+		}
+
+		public void GetHit()
+		{
+			if(_animator)
+			{
+				_animator.SetTrigger(HitTrigger);
+			}
+		}
+
+		public void SetDirection(bool facingLeft)
+		{
+			_spriteRenderer.flipX = facingLeft;
+		}
    }
 }
