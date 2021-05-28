072af27ed85c8e7e3b5fb30244fb76a065c0eba8
Jeff Rosenberg <jeff0rosenberg@gmail.com>
2021-05-27 21:22:14 -0700

    Move previews.

--- Assets/Scripts/Game/UI/SelectionPreviewBehaviour.cs
+++ Assets/Scripts/Game/UI/SelectionPreviewBehaviour.cs
@@ -1,16 +1,56 @@
+using Hats.Simulation;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace Hats.Game.UI
 {
-   public class SelectionPreviewBehaviour : MonoBehaviour, IPointerDownHandler
-   {
-      public UnityEvent OnClick;
+	public class SelectionPreviewBehaviour : MonoBehaviour, IPointerDownHandler
+	{
+		[SerializeField]
+		private SpriteRenderer previewIconSpriteRenderer;
 
-      public void OnPointerDown(PointerEventData eventData)
-      {
-         OnClick?.Invoke();
-      }
-   }
+		[Header("Icons")]
+		[SerializeField]
+		private Sprite walkPreviewSprite;
+
+		[SerializeField]
+		private Sprite fireballPreviewSprite;
+
+		[SerializeField]
+		private Sprite arrowPreviewSprite;
+
+		[Header("Events")]
+		public UnityEvent OnClick;
+
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			OnClick?.Invoke();
+		}
+
+		public void Initialize(Direction moveDirection, HatsPlayerMoveType moveType)
+		{
+			// Set the icon
+			if(previewIconSpriteRenderer)
+			{
+				switch (moveType)
+				{
+					case HatsPlayerMoveType.WALK:
+						previewIconSpriteRenderer.sprite = walkPreviewSprite;
+						break;
+					case HatsPlayerMoveType.FIREBALL:
+						previewIconSpriteRenderer.sprite = fireballPreviewSprite;
+						break;
+					case HatsPlayerMoveType.ARROW:
+						previewIconSpriteRenderer.sprite = arrowPreviewSprite;
+						break;
+					default:
+						return; // Do nothing and return
+				}
+
+				// Set the preview's direction
+				previewIconSpriteRenderer.transform.localRotation = moveDirection.GetRotation() * Quaternion.Euler(0, 0, -90);
+			}
+		}
+	}
 }
