e58b406882e3127236421693a34d884413a7a2d6
VerticalLongboard <73481831+VerticalLongboard@users.noreply.github.com>
2021-06-29 20:09:12 -0500

    Made content-referenced assets addressable again

--- Assets/Scripts/Input/MouseInputs.cs
+++ Assets/Scripts/Input/MouseInputs.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Input/MouseInputs.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/MouseInputs.inputactions'
 
 using System;
 using System.Collections;
